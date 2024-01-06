79fcdaca0c01d47ce8abe0b8ee4a4fc38b1348e2
moderatetobelowaverage <ZoeKyleson@protonmail.com>
2024-01-05 21:10:30 -0800

    Experimenting with Mixin for fancy custom crafting. Likely to be rolled back

---
+++ src/main/java/io/github/moderatetobelowaverage/vanilla_progression_reworked/mixin/BlastFurnaceMixin.java
@@ -0,0 +1,8 @@
+package io.github.moderatetobelowaverage.vanilla_progression_reworked.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+
+@Mixin(targets = "net/minecraft/block/entity/BlastFurnaceBlockEntity")
+public class BlastFurnaceMixin {
+}
